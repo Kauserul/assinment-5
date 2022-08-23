3d2953eb86a6d357ad8575a56c47b218aba666e6
Kauserul <kauserulislam0055@gmail.com>
2022-08-23 21:58:28 +0600

    cost ad

--- script.js
+++ script.js
@@ -41,3 +41,48 @@ function getPlayerName(element){
 
    
 }
+
+
+document.getElementById('per-player-cost').addEventListener('click', function(){
+    const input = document.getElementById('input-field');
+    const inputValue = parseFloat(input.value);
+    
+    if(isNaN(inputValue)){
+        alert('Pleass Enter The Amount');
+        return;
+    }
+
+    const expences = document.getElementById('player-expenses');
+    const playerExpance = playerArray.length * inputValue;
+    expences.innerText = playerExpance
+    
+});
+
+document.getElementById('total-cost').addEventListener('click', function(){
+    const manager = document.getElementById('manage-cost');
+    const managerCost = parseFloat(manager.value);
+    
+    if(isNaN(managerCost)){
+        alert('Please Enter The Amount');
+        return;
+    }
+
+    const coach = document.getElementById('coach-cost');
+    const coachCost = parseFloat(coach.value);
+    
+    if(isNaN(coachCost)){
+        alert('Please Enter The Amount');
+        return;
+    }
+
+    const expences = document.getElementById('player-expenses');
+    const totalExpences = parseFloat(expences.innerText);
+
+    const total = document.getElementById('total')
+    
+    const totalCost = totalExpences + managerCost + coachCost;
+    total.innerText = totalCost
+    
+    
+    
+})
